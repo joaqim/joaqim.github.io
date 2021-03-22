f137e0196e0e942ab4d3082a7d07776ba67475f9
Joaqim Planstedt <git@joaqim.xyz>
2021-03-22 17:59:44 +0100

    updated content

--- content/Voronoi-Sphere-article.rst
+++ content/Voronoi-Sphere-article.rst
@@ -3,18 +3,24 @@ Voronoi Sphere
 
 :date: 2019-02-26 16:16
 :category: article
-:tags: voronoi sphere,magnum,corrade,github
 :summary: Create and Render a Voronoi Sphere.
+:tags: voronoi sphere,magnum,corrade,github
 
-  `Voronoi Sphere </VoronoiSphere>`_
 
-  Create and Render a Voronoi Sphere using:
 
-  `github.com/xw0/spherical_voronoi_core <https://github.com/xw0/spherical_voronoi_core>`_
+`Emscripten Application </VoronoiSphere>`_
 
-  `github.com/mosra/magnum <https://github.com/mosra/magnum>`_
+Source: `Github <https://github.com/joaqim/VoroniSphere>`_
 
-  `github.com/mosra/corrade <https://github.com/mosra/corrade>`_
+Create and Render a Voronoi Sphere using:
+
+`github.com/xw0/spherical_voronoi_core <https://github.com/xw0/spherical_voronoi_core>`_
+
+`github.com/mosra/magnum <https://github.com/mosra/magnum>`_
+
+`github.com/mosra/corrade <https://github.com/mosra/corrade>`_
+
+
 
 
 .. raw:: html
--- pelicanconf.py
+++ pelicanconf.py
@@ -2,29 +2,27 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Joaqim Planstedt'
-SITENAME = 'www.joaqim.xyz'
-SITEURL = 'www.joaqim.xyz'
-PATH = 'content'
+AUTHOR = "Joaqim Planstedt"
+SITENAME = "www.joaqim.xyz"
+SITEURL = "www.joaqim.xyz"
+PATH = "content"
 
 
 # Blog
-#M_BLOG_NAME = 'Your Brand Blog'
-#M_BLOG_URL = 'blog/'
-#M_BLOG_FAVICON = ('favicon-blog.png', 'image/png')
-STATIC_PATHS = [
-        'extra'
-        ]
+# M_BLOG_NAME = 'Your Brand Blog'
+# M_BLOG_URL = 'blog/'
+# M_BLOG_FAVICON = ('favicon-blog.png', 'image/png')
+STATIC_PATHS = ["extra"]
 EXTRA_PATH_METADATA = {
-        'extra/CNAME': {'path': 'CNAME'},
-        }
+    "extra/CNAME": {"path": "CNAME"},
+}
 
-M_FAVICON = ('favicon.ico', 'image/x-ico')
+M_FAVICON = ("favicon.ico", "image/x-ico")
 
 
-TIMEZONE = 'Europe/Stockholm'
+TIMEZONE = "Europe/Stockholm"
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = "en"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -34,54 +32,64 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+    ("Pelican", "http://getpelican.com/"),
+    ("Python.org", "http://python.org/"),
+    ("Jinja2", "http://jinja.pocoo.org/"),
+    ("You can modify those links in your config file", "#"),
+)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
 
 DEFAULT_PAGINATION = 10
 
 USE_FOLDER_AS_CATEGORY = True
-PAGE_PATHS = ['pages']
+PAGE_PATHS = ["pages"]
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
 
 # Top navbar
-M_SITE_LOGO_TEXT = 'Joaqim.xyz'
+M_SITE_LOGO_TEXT = "Joaqim.xyz"
 
 M_LINKS_NAVBAR1 = [
-            #('Features', 'features/', 'features', []),
-            #('Showcase', 'showcase/', 'showcase', []),
-            ('UL', 'ul/', 'ul', []),
-            ('Kyuu', 'pages/kyuu', 'kyuu', []) ]
+    # ('Features', 'features/', 'features', []),
+    # ('Showcase', 'showcase/', 'showcase', []),
+    ("UL", "ul/", "ul", []),
+    ("CV", "cv/", "cv", []),
+    ("Kyuu", "pages/kyuu", "kyuu", []),
+]
 
-#M_LINKS_NAVBAR2 = [('Blog', 'blog/', '[blog]', [
-    #('News', 'blog/news/', ''),
-    #('Archive', 'blog/archive/', '')]),
-    #('Contact', 'contact/', 'contact', [])]
+# M_LINKS_NAVBAR2 = [('Blog', 'blog/', '[blog]', [
+# ('News', 'blog/news/', ''),
+# ('Archive', 'blog/archive/', '')]),
+# ('Contact', 'contact/', 'contact', [])]
 
 
+THEME = "m.css/pelican-theme"
+THEME_STATIC_DIR = "static"
+DIRECT_TEMPLATES = ["index"]
 
-THEME = 'm.css/pelican-theme'
-THEME_STATIC_DIR = 'static'
-DIRECT_TEMPLATES = ['index']
+M_CSS_FILES = [
+    "https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600,600i%7CSource+Code+Pro:400,400i,600",
+    "/static/m-dark.css",
+]
+M_THEME_COLOR = "#22272e"
 
-M_CSS_FILES = ['https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600,600i%7CSource+Code+Pro:400,400i,600',
-        '/static/m-dark.css']
-M_THEME_COLOR = '#22272e'
-
-PLUGIN_PATHS = [ 'm.css/plugins/m' ]
-PLUGINS = ['m.htmlsanity', 'm.components' ]
+PLUGIN_PATHS = ["m.css/plugins"]
+PLUGINS = ["m.htmlsanity", "m.components"]
 
 M_HTMLSANITY_SMART_QUOTES = True
 M_HTMLSANITY_HYPHENATION = True
 
-M_FINE_PRINT = SITENAME + """. Powered by `Pelican <https://getpelican.com>`_
+M_FINE_PRINT = (
+    SITENAME
+    + """. Powered by `Pelican <https://getpelican.com>`_
     and `m.css <https://mcss.mosra.cz>`_."""
+)
 
-#M_FINE_PRINT = None
+# M_FINE_PRINT = None
